c9986ed62246adba8e220726adc664e58cad9a00
stephanielinktic <stephanie.cruz@linktic.com>
2025-01-15 15:14:42 -0500

    inicial

---
+++ ad-l11-1.0/js/calculator/calculator.js
@@ -0,0 +1,17 @@
+const add = function(a, b) {
+    return a + b;
+}
+const subtract = function(a, b) {
+  return a - b;
+}
+
+function divide(a, b){
+  return a / b;
+}
+
+function multiply(a, b){
+  return a * b;
+}
+
+// When you want to use the divide or multiple function, remember to add it to the export here
+module.exports = {add: add, subtract: subtract};
